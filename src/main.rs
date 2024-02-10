02db7a9d235348db1287f62fab19d5ed78bd5af7
Chen Liang <chen.liang@ie.ibm.com>
2024-02-10 22:10:33 +0000

    refactoring for better readability

--- src/main.rs
+++ src/main.rs
@@ -48,23 +48,25 @@ fn main() {
     let input_file_path = Path::new(&args[1]);
     print!("./{}... ", input_file_path.to_str().unwrap());
 
-    let output_file_path = Path::new(&args[3]);
-
     //  Read data from file into vectors pointers
     let (x_ptr, y_ptr) = match file_ops::read_data(input_file_path) {
-        Ok((x_ptr, y_ptr)) => (x_ptr, y_ptr),
+        Ok((x_ptr, y_ptr)) => {
+            println!("OK");
+            (x_ptr, y_ptr)
+        }
         Err(e) => {
             eprintln!("{}", e.get_ref().unwrap());
             std::process::exit(exitcode::IOERR);
         }
     };
-    println!("OK");
 
     let result_x = mean_normal::features(&x_ptr.to_vec());
     let result_y = mean_normal::results(&y_ptr);
 
     //  Write to file
+    let output_file_path = Path::new(&args[3]);
     print!("Writting data to file... ");
+
     match file_ops::write_data(&result_x, &result_y, output_file_path) {
         Ok(_) => println!("OK"),
         Err(e) => {
